f49048ca631c5d1e6851caf19888965176eaab4a
Jose Ballesteros <josebp0107@gmail.com>
2021-05-07 18:21:40 -0500

    agregando estructuras de control for

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, request, make_response, redirect, render_template # Imp
 
 app = Flask(__name__) # Nueva instancia de Flask, donde le tenemos que pasar el nombre de nuestra aplicación, que en este caso seria main.py
 
+todos = ['Todo1','Todo2','Todo3']
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -15,5 +18,9 @@ def index():
 @app.route('/hello') # route recive como parametro la ruta donde queramos que se corra esta función
 def hello_world():
     user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
 
-    return render_template('hello.html', user_ip=user_ip)
+    return render_template('hello.html', **context)
