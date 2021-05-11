97f253fac912c11c8ee9c9d5905807843c8a4431
Jose Ballesteros <josebp0107@gmail.com>
2021-05-10 19:26:22 -0500

    implementando metodo session para encriptar la sesion

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, make_response, redirect, render_template # Importamos desde flask, la clase Flask para realizar nuevas instancias
+from flask import Flask, request, make_response, redirect, render_template, session # Importamos desde flask, la clase Flask para realizar nuevas instancias
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__) # Nueva instancia de Flask, donde le tenemos que pasar el nombre de nuestra aplicación, que en este caso seria main.py
 bootstrap = Bootstrap(app) # Tenemos accesos de css y js de bootstrap
+app.config['ENV'] = 'development'
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO' # Nos ayuda a generar nuestra llave secreta 
 
 todos = ['Estudiar','Estudiar estructuras de datos','Desarrollar tesis']
 
@@ -20,15 +23,14 @@ def internal_server(error):
 @app.route('/')
 def index():
     user_ip = request.remote_addr
-
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip',user_ip)
+    session['user_ip'] = user_ip # Guardamos la ip de forma segura, genera un codigo encriptado 
 
     return response
 
 @app.route('/hello') # route recive como parametro la ruta donde queramos que se corra esta función
 def hello_world():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos': todos,
